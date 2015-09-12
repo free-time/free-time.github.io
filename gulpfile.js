6dcc29f0bb382661f1b4106ed58f9896cb87439a
Anderson Menezes <andersomxp@gmail.com>
2015-09-12 00:01:22 -0300

    plugin concat javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
 var gulp = require ( 'gulp' );
-gulp.task('default', function(){
-	console.log('Hello Gulp :)');
+var concat = require('gulp-concat');
+var notify = require( 'gulp-notify' );
+
+gulp.task( 'scripts', function(){
+	gulp.src( './assets/js/*.js' )
+	.pipe( concat( 'main.js' ) )
+	.pipe( gulp.dest('./app/js/') )
+	.pipe( notify( 'Minificado com sucesso!!!' ) );
 });
+
+
+gulp.task( 'watch', function(){
+	gulp.watch( '_site/assets/js/*.js', ['scripts'] );
+});
+
+gulp.task( 'default', ['watch'] );
