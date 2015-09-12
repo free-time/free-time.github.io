d957d904b7491148b044da6da01c81d6f71ff1c4
Anderson Menezes <andersomxp@gmail.com>
2015-09-12 18:31:35 -0300

    minify javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require ( 'gulp' );
+var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var notify = require( 'gulp-notify' );
 
 gulp.task( 'scripts', function(){
 	gulp.src( './assets/js/*.js' )
 	.pipe( concat( 'main.js' ) )
+	.pipe(uglify())
 	.pipe( gulp.dest('./app/js/') )
 	.pipe( notify( 'Minificado com sucesso!!!' ) );
 });
