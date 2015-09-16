808cc0fb36024c9980ed3edaf02d05db8c766889
Anderson Menezes <andersomxp@gmail.com>
2015-09-16 10:56:52 -0300

    Plugin minify image gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require ( 'gulp' );
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
 var notify = require( 'gulp-notify' );
 
 gulp.task( 'scripts', function(){
@@ -11,9 +12,16 @@ gulp.task( 'scripts', function(){
 	.pipe( notify( 'Minificado com sucesso!!!' ) );
 });
 
+gulp.task( 'imagemin', function(){
+	gulp.src('assets/image/**/*.{jpg,png,svg}')
+	.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+	.pipe(gulp.dest('app/image'))
+	.pipe( notify( 'Imagens compreessas com sucesso!!!' ) );
+});
 
 gulp.task( 'watch', function(){
 	gulp.watch( '_site/assets/js/*.js', ['scripts'] );
+	gulp.watch( 'assets/image/**/*.{jpg,png,svg}', ['imagemin'] );
 });
 
 gulp.task( 'default', ['watch'] );
