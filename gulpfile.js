45e5c8abf012c415e7f590e08d5ebfc0ea45c178
Anderson Menezes <andersomxp@gmail.com>
2015-10-27 12:01:26 -0300

    remove gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('sass', function () {
     }))
     .pipe(gulp.dest('_site/assets/css'))
     .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest('_site/assets/css'));
+    .pipe(gulp.dest('/assets/css'));
 });
 
 gulp.task( 'imagemin', function(){
