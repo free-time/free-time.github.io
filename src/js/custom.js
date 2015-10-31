44c508e49109a0ef67f2080ba7f408e784325c09
Anderson Menezes <andersomxp@gmail.com>
2015-10-30 22:30:00 -0300

    custom

--- src/js/custom.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	function limitChars () {
-		var titleTalk = document.querySelector('.lecture-title').textContent;
-		if ( titleTalk > 10 ) {
-			var minTitle = titleTalk.substring(0,10);
-			titleTalk.innerHTML = minTitle + '...';
-		}
-	}
-}());
