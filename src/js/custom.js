365e8c36f6d12a3b7c0f01520d0e347db65d592e
Anderson Menezes <andersomxp@gmail.com>
2015-11-21 00:35:09 -0300

    Changed limit charactrs

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,7 +3,7 @@
     function limitChars(){
       var $titleTalk = document.querySelectorAll('.lecture-title');
       Array.prototype.forEach.call($titleTalk,function(element,item){
-        if( $titleTalk[item].innerHTML.length > 56 ){
+        if( $titleTalk[item].innerHTML.length > 50 ){
           $titleTalk[item].innerHTML = $titleTalk[item].innerHTML.substring(0,50) + '...';
         }
       });
